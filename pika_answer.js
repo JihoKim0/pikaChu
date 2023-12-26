e47ec49ccd75d652b756fb5a21fb0d54c4c4553d
JihoKim0 <79587909+JihoKim0@users.noreply.github.com>
2023-12-27 08:27:45 +0900

    Add files via upload

---
+++ pika_answer.js
@@ -0,0 +1,45 @@
+/*
+Pikachu.EXE
+1P:왼쪽 플레이어, 2P:오른쪽 플레이어
+1. 무조건 2P가 점수먹기
+2. 1P 점수조작(바로 15점)
+3. 2P 점수조작(바로 15점)
+
+*/
+
+1.
+var pika = ptr(0x403C47);
+Interceptor.attach(pika, {
+	onEnter: function (args) {
+		this.context.eax = 0x1;	//2P
+		this.context.eax = 0x0;	//1P
+	},
+	onLeave: function (retval) {
+	},
+});
+
+2.
+var pika2 = ptr(0x403C54);
+Interceptor.attach(pika2, {
+	onEnter: function (args) {
+		this.context.ecx = 0xf;
+		
+	},
+	onLeave: function (retval) {
+	},
+});
+
+
+3.
+var pika3 = ptr(0x403C47);
+Interceptor.attach(pika3, {
+	onEnter: function (args) {
+		var score = this.context.esi;
+		/*Memory.protect(ptr(score), 16, 'rwx');*/
+		/*console.log(ptr(score).add(64).readPointer()); //2P 점수*/
+		Memory.writeByteArray(ptr(score).add(64), [0xf]);
+		
+	},
+	onLeave: function (retval) {
+	},
+});
